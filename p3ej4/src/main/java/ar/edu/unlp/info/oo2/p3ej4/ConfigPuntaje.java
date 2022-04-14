0e0490d699370ba10fd841c7adfb98b3e0c9d182
Gregorio Maclen <gregorio.maclen@gmail.com>
2022-04-14 18:57:48 -0300

    .

--- p3ej4/src/main/java/ar/edu/unlp/info/oo2/p3ej4/ConfigPuntaje.java
+++ p3ej4/src/main/java/ar/edu/unlp/info/oo2/p3ej4/ConfigPuntaje.java
@@ -1,5 +1,6 @@
 package ar.edu.unlp.info.oo2.p3ej4;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class ConfigPuntaje implements ConfigDeRecom{
@@ -7,7 +8,7 @@ public class ConfigPuntaje implements ConfigDeRecom{
 	public List<Pelicula> getSugerencias(Decodificador d) {
 		return d.getPeliculasTodas().stream().
 				filter(p -> !d.getPeliculasQueReprodujo().contains(p)).
-				sorted((p1, p2) -> p1.getPuntaje()).
+				sorted(Comparator.comparingDouble(Pelicula::getPuntaje)).
 				toList().
 				subList(0, 3);
 	}
--- p3ej4/src/main/java/ar/edu/unlp/info/oo2/p3ej4/ConfigSimilaridad.java
+++ p3ej4/src/main/java/ar/edu/unlp/info/oo2/p3ej4/ConfigSimilaridad.java
@@ -1,11 +1,21 @@
 package ar.edu.unlp.info.oo2.p3ej4;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigSimilaridad implements ConfigDeRecom{
 	@Override
 	public List<Pelicula> getSugerencias(Decodificador d) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Pelicula> similares = new ArrayList<Pelicula>();
+		for (Pelicula pelicula : d.getPeliculasQueReprodujo()) {
+			for (Pelicula pelicula2 : pelicula.getSimilares()) {
+				if (!similares.contains(pelicula2))
+					similares.add(pelicula2);
+			}
+		}
+		return similares.stream().
+				filter(p -> !d.getPeliculasQueReprodujo().contains(p)).
+				toList().
+				subList(0, 3);
 	}
 }
