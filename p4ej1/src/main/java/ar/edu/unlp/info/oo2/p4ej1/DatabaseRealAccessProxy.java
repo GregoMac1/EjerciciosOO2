76949a8435d54bb093a0fcd0425d4867268aa6ab
Gregorio Maclen <gregorio.maclen@gmail.com>
2022-04-18 12:27:10 -0300

    .

---
+++ p4ej1/src/main/java/ar/edu/unlp/info/oo2/p4ej1/DatabaseRealAccessProxy.java
@@ -0,0 +1,31 @@
+package ar.edu.unlp.info.oo2.p4ej1;
+
+import java.util.Collection;
+import java.util.List;
+
+public class DatabaseRealAccessProxy implements DatabaseAccess{
+	private DatabaseAccess database;
+	private boolean isLogged;
+
+	public DatabaseRealAccessProxy(DatabaseAccess database) {
+		this.database = database;
+	}
+
+    @Override
+    public Collection<String> getSearchResults(String queryString) {
+        if (!this.checkAccess())
+        	throw new RuntimeException("Acceso denegado");
+        return database.getSearchResults(queryString);
+    }
+
+    @Override
+    public int insertNewRow(List<String> rowData) {
+    	if (!this.checkAccess())
+        	throw new RuntimeException("Acceso denegado");
+        return database.insertNewRow(rowData);
+    }
+    
+    private boolean checkAccess() {
+    	return this.isLogged;
+    }
+}
